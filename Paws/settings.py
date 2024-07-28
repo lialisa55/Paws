a3d1db1ad361a4a1d4f44c5641837269532975d7
lialisa55 <nathaliamonalisa150@gmail.com>
2024-07-28 01:48:28 -0300

    adicionando banco de dados para animais e resgatadores

--- Paws/settings.py
+++ Paws/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Internationalization
--- vitrine/models.py
+++ vitrine/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Resgatador(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+class Animal(models.Model):
+    SEXO_CHOICES = [
+        ('M', 'Macho'),
+        ('F', 'Fêmea'),
+    ]
+
+    PORTE_CHOICES = [
+        ('P', 'Pequeno'),
+        ('M', 'Médio'),
+        ('G', 'Grande'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    idade_aproximada = models.IntegerField
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    microchip = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    especie = models.CharField(max_length=50)
+    porte = models.CharField(max_length=1, choices=PORTE_CHOICES)
+    raca = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2)
+    cidade = models.CharField(max_length=100)
+    sobre_o_pet = models.TextField()
+    adotado = models.BooleanField(default=False)
+    foto = models.ImageField(upload_to='fotos_animais/', null=True, blank=True)
+    resgatador = models.ForeignKey(Resgatador, on_delete=models.CASCADE, related_name='animais')
+    data_resgate = models.DateField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nome} ({self.especie})"
