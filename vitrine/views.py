5af090ab277cceb9c2fb60b1eb1edd8111471cb1
lialisa55 <nathaliamonalisa150@gmail.com>
2024-07-30 00:09:59 -0300

    html da vitrine acessívelna página vitrine/

--- vitrine/views.py
+++ vitrine/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Resgatador, Animal
 
 # Create your views here.
 
 def vitrine(request):
-    return HttpResponse("Você está na vitrine")
+    lista_animais = Animal.objects.order_by("-data_atualizacao")[:5]
+    template = loader.get_template("vitrine/vitrine.html")
+    context = {
+            "lista_animais": lista_animais,
+    }
+    return HttpResponse(template.render(context, request))
 
