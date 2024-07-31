e105008449275167814fb71f5820a28817cd13e0
lialisa55 <nathaliamonalisa150@gmail.com>
2024-07-31 00:22:44 -0300

    imagens em home funcionais

--- Paws/settings.py
+++ Paws/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'home/static'),
+        os.path.join(BASE_DIR, 'vitrine/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.say_hello)
+        path('', views.home)
 ]
